8ba5f214a930e888da15bb77a056f0ce8301a77a
eugene-afanasyev <whoaeugene@gmail.com>
2021-03-12 23:50:33 +1000

    Vec2 copy and move constructors realised and covered with tests

--- src/math/vec2.cpp
+++ src/math/vec2.cpp
@@ -5,3 +5,13 @@ Vec2::Vec2() : x(0.0f), y(0.0f) {}
 Vec2::Vec2(float value) : x(value), y(value) {}
 
 Vec2::Vec2(float x, float y) : x(x), y(y) {}
+
+Vec2::Vec2(const Vec2 &src) {
+  x = src.x;
+  y = src.y;
+}
+
+Vec2::Vec2(Vec2 &&src) {
+  x = src.x;
+  y = src.y;
+}
--- test/test.cpp
+++ test/test.cpp
@@ -16,6 +16,7 @@ TEST_CASE("Vec2 initialization") {
   {
     glm::vec2 glm_vec{};
     Vec2 custom_vec;
+
     REQUIRE(glm_vec.x == custom_vec.x);
     REQUIRE(glm_vec.y == custom_vec.y);
   }
@@ -23,6 +24,7 @@ TEST_CASE("Vec2 initialization") {
   {
     glm::vec2 glm_vec(1.1f);
     Vec2 custom_vec(1.1f);
+
     REQUIRE(glm_vec.x == custom_vec.x);
     REQUIRE(glm_vec.y == custom_vec.y);
   }
@@ -30,7 +32,33 @@ TEST_CASE("Vec2 initialization") {
   {
     glm::vec2 glm_vec(3.14f, 1.77f);
     Vec2 custom_vec(3.14f, 1.77f);
+
     REQUIRE(glm_vec.x == custom_vec.x);
     REQUIRE(glm_vec.y == custom_vec.y);
+  }
+}
+
+TEST_CASE("Vec2 copy and move constructors") {
+  {
+    Vec2 dst_vec(1.1f, 3.14f);
+    Vec2 src_vec(dst_vec);
+
+    REQUIRE(src_vec.x == dst_vec.x);
+    REQUIRE(src_vec.y == dst_vec.y);
+  }
+
+  {
+    Vec2 dst_vec(0.01f);
+    Vec2 src_vec(dst_vec);
+
+    REQUIRE(src_vec.x == dst_vec.x);
+    REQUIRE(src_vec.y == dst_vec.y);
+  }
+
+  {
+    Vec2 vec(Vec2(1.14f, 3.28f));
+
+    REQUIRE(floatCmp(vec.x, 1.14f));
+    REQUIRE(floatCmp(vec.y, 3.28f));
   }
 }
