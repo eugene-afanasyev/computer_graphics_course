3fe2c4a92f2d5ceea0e500f00f0a692f65e332c8
eugene-afanasyev <whoaeugene@gmail.com>
2021-04-06 22:54:00 +1000

    Mat4 accessors defined

--- src/math/mat4.cpp
+++ src/math/mat4.cpp
@@ -1,4 +1,5 @@
 #include "mat4.hpp"
+#include "exception.hpp"
 
 using vec_type = cglm::Vec4;
 
@@ -35,4 +36,35 @@ Mat4::Mat4(const vec_type &r0, const vec_type &r1, const vec_type &r2, const vec
   rows_[1] = r1;
   rows_[2] = r2;
   rows_[3] = r3;
+}
+
+vec_type Mat4::get_row(uint32_t r) const {
+  if (r < 0u || r > 3u)
+    throw cglm::OutOfRangeException();
+  else
+    return rows_[r];
+}
+
+vec_type Mat4::get_col(uint32_t c) const {
+  if (c < 0u || c > 3u)
+    throw cglm::OutOfRangeException();
+  else if (c == 0)
+    return {rows_[0].x, rows_[1].x, rows_[2].x, rows_[3].x};
+  else if (c == 1)
+    return {rows_[0].y, rows_[1].y, rows_[2].y, rows_[3].y};
+  else if (c == 2)
+    return {rows_[0].z, rows_[1].z, rows_[2].z, rows_[3].z};
+  else
+    return {rows_[0].w, rows_[1].w, rows_[2].w, rows_[3].w};
+}
+
+vec_type Mat4::get_diag() const noexcept {
+  return {rows_[0].x, rows_[1].y, rows_[2].z, rows_[3].w};
+}
+
+vec_type Mat4::operator[](unsigned int r) const {
+  if (r < 0u || r > 3u)
+    throw cglm::OutOfRangeException();
+  else
+    return rows_[r];
 }
