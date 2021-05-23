6e234414a5018ca87403e931b1752d74228547cf
eugene-afanasyev <whoaeugene@gmail.com>
2021-05-24 01:37:44 +1000

    Defined ShaderWrapper class

--- src/shader_wrapper.cpp
+++ src/shader_wrapper.cpp
@@ -1,12 +1,79 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <GL/glew.h>
 #include "shader_wrapper.h"
-#include "shaderLoader.h"
 
-ShaderWrapper::ShaderWrapper(const std::string &vertex_file_path,
-                             const std::string &fragment_file_path) {
-  shader_id_ = LoadShaders(vertex_file_path, fragment_file_path);
+ShaderWrapper::ShaderWrapper(const std::string &vs_path, const std::string &fs_path) {
+  std::string raw_vs_code;
+  std::string raw_fs_code;
+
+  std::ifstream vs_code_src;
+  std::ifstream fs_code_src;
+
+  vs_code_src.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  fs_code_src.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+  try {
+    vs_code_src.open(kShadersFolderPath_ + vs_path);
+    fs_code_src.open(kShadersFolderPath_ + fs_path);
+
+    std::stringstream vs_stream, fs_stream;
+
+    vs_stream << vs_code_src.rdbuf();
+    fs_stream << fs_code_src.rdbuf();
+
+    vs_code_src.close();
+    fs_code_src.close();
+
+    raw_vs_code = vs_stream.str();
+    raw_fs_code = fs_stream.str();
+  } catch (std::ifstream::failure &e) {
+    std::cout << "ERROR::SHADER_LOADER: " << e.what() << std::endl;
+  }
+  const char *vertex_shader_code = raw_vs_code.c_str();
+  const char *fragment_shader_code = raw_fs_code.c_str();
+
+  GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vertex_shader_code, nullptr);
+  glCompileShader(vertex_shader);
+  CheckShaderCompileErrors_(vertex_shader, "VERTEX");
+
+  GLuint fragment_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_code, nullptr);
+  glCompileShader(fragment_shader);
+  CheckShaderCompileErrors_(fragment_shader, "FRAGMENT");
+
+  id_ = glCreateProgram();
+  glAttachShader(id_, vertex_shader);
+  glAttachShader(id_, fragment_shader);
+  glLinkProgram(id_);
+  CheckShaderCompileErrors_(id_, "PROGRAM");
+
+  glDeleteShader(vertex_shader);
+  glDeleteShader(fragment_shader);
 }
 
 void ShaderWrapper::Use() const {
-  glUseProgram(shader_id_);
+  glUseProgram(id_);
+}
+
+void ShaderWrapper::CheckShaderCompileErrors_(GLuint shader_id, const std::string &type) const {
+  int success;
+  char info_log[1024];
+  if (type != "PROGRAM") {
+    glGetShaderiv(shader_id, GL_COMPILE_STATUS, &success);
+    if (!success) {
+      glGetShaderInfoLog(shader_id, 1024, nullptr, info_log);
+      std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << info_log <<
+      "\n -- --------------------------------------------------- -- " << std::endl;
+    }
+  } else {
+    glGetProgramiv(shader_id, GL_LINK_STATUS, &success);
+    if (!success) {
+      glGetProgramInfoLog(shader_id, 1024, nullptr, info_log);
+      std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << info_log
+      << "\n -- --------------------------------------------------- -- " << std::endl;
+    }
+  }
 }
--- src/shader_wrapper.h
+++ src/shader_wrapper.h
@@ -6,14 +6,21 @@
 
 class ShaderWrapper {
  public:
-   explicit ShaderWrapper(const std::string&, const std::string&);
+   explicit ShaderWrapper(const std::string& vs_path, const std::string& fs_path);
 
   ~ShaderWrapper() = default;
 
   void Use() const;
 
+  // utility uniform functions
+  void setBool(const std::string &name, bool value) const;
+  void setInt(const std::string &name, int value) const;
+  void setFloat(const std::string &name, float value) const;
  private:
-  GLuint shader_id_;
+  GLuint id_;
+  const std::string kShadersFolderPath_ = "/home/eugene/CLionProjects/computer-graphics-course/res/shaders";
+
+  void CheckShaderCompileErrors_(GLuint shader_id, const std::string& type) const;
 };
 
 #endif //COMPUTER_GRAPHICS_COURSE_SRC_SHADER_WRAPPER_H_
